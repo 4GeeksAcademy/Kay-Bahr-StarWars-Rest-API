65b4efbf5c9ff7ecc074da24ca96f440ce6e9dc8
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-05-20 00:15:33 +0000

    instruct commit

--- src/app.py
+++ src/app.py
@@ -49,3 +49,131 @@ def handle_hello():
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+
+# # # Create an API that connects to a database and implements the following Endpoints (very similar to SWAPI.dev or SWAPI.tech):
+
+# # [GET] /people Get a list of all the people in the database
+
+    # @app.route('/user', methods=['GET'])
+    # def handle_user():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [GET] /people/<int:people_id> Get a one single people information
+
+    # @app.route('/user', methods=['GET'])
+    # def handle_people():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [GET] /planets Get a list of all the planets in the database
+
+    # @app.route('/user', methods=['GET'])
+    # def handle_planets():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [GET] /planets/<int:planet_id> Get one single planet information
+
+    # @app.route('/user', methods=['GET'])
+    # def handle_planet():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # Additionally create the following endpoints to allow your StartWars blog to have users and favorites:
+
+# # [GET] /users Get a list of all the blog post users
+
+    # @app.route('/user', methods=['GET'])
+    # def handle_users():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [GET] /users/favorites Get all the favorites that belong to the current user.
+
+    # @app.route('/user', methods=['GET'])
+    # def handle_favorites():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [POST] /favorite/planet/<int:planet_id> Add a new favorite planet to the current user with the planet id = planet_id.
+
+    # @app.route('/user', methods=['POST'])
+    # def create_favorite_planet():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [POST] /favorite/people/<int:people_id> Add new favorite people to the current user with the people id = people_id.
+
+    # @app.route('/user', methods=['POST'])
+    # def create_favorite_people():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [DELETE] /favorite/planet/<int:planet_id> Delete favorite planet with the id = planet_id.
+
+    # @app.route('/user', methods=['DELETE'])
+    # def delete_favorite_planet():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # [DELETE] /favorite/people/<int:people_id> Delete favorite people with the id = people_id.
+
+    # @app.route('/user', methods=['DELETE'])
+    # def delete_favorite_people():
+
+    #     response_body = {
+    #         
+    #     }
+
+    #     return jsonify(response_body), 200
+
+
+# # Your current API does not have an authentication system (yet), which is why the only way to create users is directly on the database using the flask admin.
