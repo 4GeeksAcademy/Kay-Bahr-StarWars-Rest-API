d68e0f58f0c94cecef45821c505b04c5bf4edc50
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-05-20 02:29:47 +0000

    model first commit

--- src/app.py
+++ src/app.py
@@ -45,135 +45,154 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
-
-
-
-# # # Create an API that connects to a database and implements the following Endpoints (very similar to SWAPI.dev or SWAPI.tech):
+# # Create an API that connects to a database and implements the following Endpoints (very similar to SWAPI.dev or SWAPI.tech):
 
 # # [GET] /people Get a list of all the people in the database
 
-    # @app.route('/user', methods=['GET'])
-    # def handle_user():
+@app.route('/people', methods=['GET'])
+def handle_people():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your GET /people response "
+    }
 
-    #     return jsonify(response_body), 200
-
+    return jsonify(response_body), 200
 
 # # [GET] /people/<int:people_id> Get a one single people information
 
-    # @app.route('/user', methods=['GET'])
-    # def handle_people():
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_people():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your GET /people/<int:people_id> response "
+    }
 
-    #     return jsonify(response_body), 200
-
+    return jsonify(response_body), 200
 
 # # [GET] /planets Get a list of all the planets in the database
 
-    # @app.route('/user', methods=['GET'])
-    # def handle_planets():
+@app.route('/planets', methods=['GET'])
+def handle_planets():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your GET /planets response "
+    }
 
-    #     return jsonify(response_body), 200
-
+    return jsonify(response_body), 200
 
 # # [GET] /planets/<int:planet_id> Get one single planet information
 
-    # @app.route('/user', methods=['GET'])
-    # def handle_planet():
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_planet():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your GET /planets/<int:planet_id> response "
+    }
 
-    #     return jsonify(response_body), 200
+    return jsonify(response_body), 200
 
+# # [GET] /starships Get a list of all the starships in the database
+
+@app.route('/starships', methods=['GET'])
+def handle_starships():
+
+    response_body = {
+        "msg": "Hello, this is your GET /starships response "
+    }
+
+    return jsonify(response_body), 200
+
+# # [GET] /starships/<int:starship_id> Get one single starship information
+
+@app.route('/starships/<int:starship_id>', methods=['GET'])
+def handle_planet():
+
+    response_body = {
+        "msg": "Hello, this is your GET /starships/<int:starship_id> response "
+    }
+
+    return jsonify(response_body), 200
 
 # # Additionally create the following endpoints to allow your StartWars blog to have users and favorites:
 
 # # [GET] /users Get a list of all the blog post users
 
-    # @app.route('/user', methods=['GET'])
-    # def handle_users():
+@app.route('/user', methods=['GET'])
+def handle_users():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
 
-    #     return jsonify(response_body), 200
-
+    return jsonify(response_body), 200
 
 # # [GET] /users/favorites Get all the favorites that belong to the current user.
 
-    # @app.route('/user', methods=['GET'])
-    # def handle_favorites():
+@app.route('/users/favorites', methods=['GET'])
+def handle_favorites():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your GET /users/favorites response "
+    }
 
-    #     return jsonify(response_body), 200
+    return jsonify(response_body), 200
 
+# # [POST] /favorite/starship/<int:starship_id> Add a new favorite starship to the current user with the starship id = starship_id.
+
+@app.route('/favorite/starship/<int:starship_id>', methods=['POST'])
+def create_favorite_starship():
+
+    response_body = {
+        "msg": "Hello, this is your POST /favorite/starship/<int:starship_id> response "
+    }
+
+    return jsonify(response_body), 200
 
 # # [POST] /favorite/planet/<int:planet_id> Add a new favorite planet to the current user with the planet id = planet_id.
 
-    # @app.route('/user', methods=['POST'])
-    # def create_favorite_planet():
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def create_favorite_planet():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your POST /favorite/planet/<int:planet_id> response "
+    }
 
-    #     return jsonify(response_body), 200
-
+    return jsonify(response_body), 200
 
 # # [POST] /favorite/people/<int:people_id> Add new favorite people to the current user with the people id = people_id.
 
-    # @app.route('/user', methods=['POST'])
-    # def create_favorite_people():
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def create_favorite_people():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your POST /favorite/people/<int:people_id> response "
+    }
 
-    #     return jsonify(response_body), 200
-
+    return jsonify(response_body), 200
 
 # # [DELETE] /favorite/planet/<int:planet_id> Delete favorite planet with the id = planet_id.
 
-    # @app.route('/user', methods=['DELETE'])
-    # def delete_favorite_planet():
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your Delete /favorite/planet/<int:planet_id> response "
+    }
 
-    #     return jsonify(response_body), 200
-
+    return jsonify(response_body), 200
 
 # # [DELETE] /favorite/people/<int:people_id> Delete favorite people with the id = people_id.
 
-    # @app.route('/user', methods=['DELETE'])
-    # def delete_favorite_people():
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people():
 
-    #     response_body = {
-    #         
-    #     }
+    response_body = {
+        "msg": "Hello, this is your DELETE /favorite/people/<int:people_id> response "
+    }
 
-    #     return jsonify(response_body), 200
+    return jsonify(response_body), 200
 
-
-# # Your current API does not have an authentication system (yet), which is why the only way to create users is directly on the database using the flask admin.
+# this only runs if `$ python src/app.py` is executed
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -16,4 +18,62 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class People():
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "type": self.type,
+        }
+
+class Planets():
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "type": self.type,
+        }
+    
+class Starships():
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Starships %r>' % self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "type": self.type,
+        }
+    
+class Favorites():
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("User"))
+    people_name = db.Column(db.String(80), db.ForeignKey("People"))
+    planets_name = db.Column(db.String(80), db.ForeignKey("Planets"))
+    starships_name = db.Column(db.String(80), db.ForeignKey("Starships"))
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_name": self.people_name,
+            "planets_name": self.planets_name,
+            "starships_name": self.starships_name,
         }
